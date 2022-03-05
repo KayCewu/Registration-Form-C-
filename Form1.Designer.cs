114a0997d08d5e6288a6f669a1cc67f44d6513e6
KayCewu <70000550+KayCewu@users.noreply.github.com>
2022-03-05 14:52:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CRegistrationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtSur = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.MsktxtNumber = new System.Windows.Forms.MaskedTextBox();
+            this.cmbNationality = new System.Windows.Forms.ComboBox();
+            this.cmbTitle = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nationality:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Identity/Passport No.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(236, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Phone Number:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(71, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(236, 29);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Title:";
+            // 
+            // txtSur
+            // 
+            this.txtSur.Location = new System.Drawing.Point(71, 64);
+            this.txtSur.Name = "txtSur";
+            this.txtSur.Size = new System.Drawing.Size(100, 20);
+            this.txtSur.TabIndex = 11;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(125, 103);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 13;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.Red;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.Black;
+            this.btnRegister.Location = new System.Drawing.Point(84, 162);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(241, 74);
+            this.btnRegister.TabIndex = 15;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MsktxtNumber
+            // 
+            this.MsktxtNumber.Location = new System.Drawing.Point(323, 103);
+            this.MsktxtNumber.Mask = "(+27) 00-000-0000";
+            this.MsktxtNumber.Name = "MsktxtNumber";
+            this.MsktxtNumber.Size = new System.Drawing.Size(100, 20);
+            this.MsktxtNumber.TabIndex = 16;
+            // 
+            // cmbNationality
+            // 
+            this.cmbNationality.FormattingEnabled = true;
+            this.cmbNationality.Location = new System.Drawing.Point(301, 64);
+            this.cmbNationality.Name = "cmbNationality";
+            this.cmbNationality.Size = new System.Drawing.Size(121, 21);
+            this.cmbNationality.TabIndex = 17;
+            // 
+            // cmbTitle
+            // 
+            this.cmbTitle.FormattingEnabled = true;
+            this.cmbTitle.Location = new System.Drawing.Point(272, 26);
+            this.cmbTitle.Name = "cmbTitle";
+            this.cmbTitle.Size = new System.Drawing.Size(121, 21);
+            this.cmbTitle.TabIndex = 18;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 248);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Close";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 283);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.cmbTitle);
+            this.Controls.Add(this.cmbNationality);
+            this.Controls.Add(this.MsktxtNumber);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtSur);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSur;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.MaskedTextBox MsktxtNumber;
+        private System.Windows.Forms.ComboBox cmbNationality;
+        private System.Windows.Forms.ComboBox cmbTitle;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+//Import the SqlClient
+using System.Data.SqlClient;
+
+namespace CRegistrationForm
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Declare the connection string
+        string SQL;
+        public string ConStrr = "Data Source=LAPTOP-4IS5CG1L;Initial Catalog=CeeRegTable;Integrated Security=True";
+        SqlConnection Con=new SqlConnection();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //When the form loads, load the following info to the comboboxes
+            cmbNationality.Items.Add("South African");
+            cmbNationality.Items.Add("Zimbabwean");
+            cmbNationality.Items.Add("Lesotho");
+            cmbNationality.Items.Add("Nigerian");
+            cmbNationality.Items.Add("Swati");
+            cmbNationality.Items.Add("Other");
+            cmbTitle.Items.Add("Mr");
+            cmbTitle.Items.Add("Mrs");
+            cmbTitle.Items.Add("Ms");
+            cmbTitle.Items.Add("Dr");
+            this.cmbTitle.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.cmbNationality.DropDownStyle = ComboBoxStyle.DropDownList;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Input validations
+            if ("".Equals(txtName.Text))
+            {
+                MessageBox.Show("Name left blank", "Error", 0, MessageBoxIcon.Error);
+                return;
+            }
+            if ("".Equals(txtID.Text))
+            {
+                MessageBox.Show("ID Number left blank", "Error", 0, MessageBoxIcon.Error);
+                return;
+            }
+            if ("".Equals(txtSur.Text))
+            {
+                MessageBox.Show("Surname left blank", "Error", 0, MessageBoxIcon.Error);
+                return;
+            }
+            if ("".Equals(cmbTitle.Text))
+            {
+                MessageBox.Show("Title left blank", "Error", 0, MessageBoxIcon.Error);
+                return;
+            }
+            if ("".Equals(cmbNationality.Text))
+            {
+                MessageBox.Show("Nationality left blank", "Error", 0, MessageBoxIcon.Error);
+                return;
+            }
+            if ("".Equals(MsktxtNumber.Text))
+            {
+                MessageBox.Show("Phone number left blank", "Error", 0, MessageBoxIcon.Error);
+                return;
+            }
+            //Create a regular expression to check for any incorrect input
+            Regex reg = new Regex("[^a-zA-Z]");
+            bool hasSpeChar = reg.IsMatch(txtName.Text.ToString()) || reg.IsMatch(txtSur.Text.ToString());
+            if (hasSpeChar == true)
+            {
+                MessageBox.Show("Cannot contain special characters or numbers", "Error", 0, MessageBoxIcon.Error);
+                return;
+            }
+            if (txtID.Text.Length != 13)
+            {
+                MessageBox.Show("ID number has to be 13 characters long", "Error", 0, MessageBoxIcon.Error);
+                txtID.Clear();
+                txtID.Focus();
+                return;
+            }
+            //We now prepare for database connection
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                SQL = "Insert into regdetails(FName, SName, IdentityNo, Title, Nationality, PhoneNo) values ('" + txtName.Text + "','" + txtSur.Text + "','" + txtID.Text + "','" + cmbTitle.Text + "','" + cmbNationality.Text + "','" + MsktxtNumber.Text + "')";
+                Con = new SqlConnection(ConStrr);
+                cmd = new SqlCommand(SQL, Con);
+                if (Con.State == ConnectionState.Closed)
+                {
+                    Con.Open();
+                }
+                cmd.CommandText = SQL;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registered Successfully", "Congratulations", 0, MessageBoxIcon.Information);
+                if (Con.State == ConnectionState.Open)
+                {
+                    Con.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Record not added", "Error", 0, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
